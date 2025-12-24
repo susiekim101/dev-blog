cf95b97587e3fea0d41da43ecb543b5920167427
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-12-24 00:23:15 -0800

    fetch posts from backend and render on screen

--- app/Blog/AllPosts/AllPosts.tsx
+++ app/Blog/AllPosts/AllPosts.tsx
@@ -1,22 +1,28 @@
+'use client';
 import PostCard from "../Recent/PostCard";
+import { useEffect, useState } from "react";
+import fetchAllPosts from "./actions";
+import type { PostData } from "@/app/admin/Form/actions";
 
 const AllPosts = () => {
+    const [ allPosts, setAllPosts ] = useState<PostData[]>([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await fetchAllPosts();
+            setAllPosts(data);
+        };
+        getPosts();
+    }, []);
 
     return (
         <section className='w-full'>
             <h1 className='text-xl font-bold mb-5'>All blog posts</h1>
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
-                <PostCard layout='vertical'/>
-                <PostCard layout='vertical'/>
-                <PostCard layout='vertical'/>
-                <PostCard layout='vertical'/>            
-                <PostCard layout='vertical'/>
-                <PostCard layout='vertical'/>            
-                <PostCard layout='vertical'/>
-                <PostCard layout='vertical'/>            
-                <PostCard layout='vertical'/>
-                <PostCard layout='vertical'/>
+                {allPosts.map((post) => {
+                    return <PostCard key={post._id} layout='vertical' post={post}/>
+                })}
             </div>
         </section>
     );
